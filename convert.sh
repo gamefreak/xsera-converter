4419f5dfff07642c7d8df145d597d7a7d4b1d59a
Scott McClaugherty <gamefreak4321@gmail.com>
2009-10-02 20:55:59 -0400

    Script now has a way to retrieve the results

--- convert.sh
+++ convert.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
-echo $1
-mkdir ./temp
+
+#mkdir ./temp
 f=./temp/${1//*\//}
 	
-rm "$f"
 cp "$1" "$f"
-
 sed -f ./converter.sed <"$f" >"$f"2
 
-tr "\n" ' ' <"$f"2 >./"$f"3
+tr "\n" ' ' <"$f"2 >"$f"3
 sed -e 's|,[^,]*$||' <"$f"3 >"$f"4
-! lua <"$file"4 2>>err && echo "$file">>err
+lua <"$f"4 2>>err && cp "${f}4" "./${f/xml/lua}"
 
-#rm "$f"*
-rmdir ./temp
+rm "$f"
+rm "$f"[1234]
+
+#rmdir ./temp

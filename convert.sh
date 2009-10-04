49f9b5ef18e593e500d117ae303d3638cab30009
Scott McClaugherty <gamefreak4321@gmail.com>
2009-10-04 19:16:19 -0400

    JACKPOT! all of the XML data file now convert to to valid lua tables.

--- convert.sh
+++ convert.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
 
-#mkdir ./temp
 f=./temp/${1//*\//}
 	
 cp "$1" "$f"
-sed -f ./converter.sed <"$f" >"$f"2
+sed -e "s|\(\"[^\"]*\), \([^\"]*\"\)|\1\s\2|g" <"$f" >"$f"1
+sed -f ./converter.sed <"$f"1 >"$f"2
 
 tr "\n" ' ' <"$f"2 >"$f"3
 sed -e 's|,[^,]*$||' <"$f"3 >"$f"4
-lua <"$f"4 2>>err && cp "${f}4" "./${f/xml/lua}"
+#sed -e 's|, |,\
+#|g' <"$f"4 >"$f"5
+cp "${f}4" "./${f/xml/lua}"
+lua "./${f/xml/lua}" 2>>err && echo "OK"
 
 rm "$f"
-rm "$f"[1234]
+rm "$f"[12345]
 
-#rmdir ./temp

014053071ab371ff991c784cdab6ccee0a2f2a02
Scott McClaugherty <gamefreak4321@gmail.com>
2009-10-14 10:10:24 -0400

    Fix an error in the process of converting weapons references.

--- run_conversions.sh
+++ run_conversions.sh
@@ -6,13 +6,13 @@ mkdir -p ./new/Actions
 mkdir -p ./new/Objects
 mkdir -p ./new/Races
 mkdir -p ./new/Scenarios
-
 if [ -n "$1" ]
 then
 	prefix=$1
 else
 	prefix="."
 fi
+#prefix="."
 find $prefix -name *.xml -type f -print0 | xargs -n 1 -0 ./convert.sh 
 
 #find ./new -name *.lua -type f -print0 | xargs -n 1 -0 lua 2>>err

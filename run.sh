7b78d8bd8cade40f02a4ec54cccf18e800ead74f
Scott McClaugherty <gamefreak4321@gmail.com>
2009-12-03 18:20:14 -0500

    Creates xml files for sprites. (does not handle rotantionals)
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- run.sh
+++ run.sh
@@ -8,16 +8,16 @@ echo "return {" > $2/data.lua
 
 #convert the XML
 
-for dir in Actions InitialObject Objects Races Scenarios
-do
-echo "$dir"
-mkdir -p "$2$dir"
-./convert.sh "$1$dir" "$2$dir"
-echo "[\"$dir\"] = {" > "$2/$dir.lua"
-cat $2$dir/* >> "$2/$dir.lua"
-echo "};" >> "$2/$dir.lua"
-cat $2/$dir.lua >> $2/data.lua
-done
+#for dir in Actions InitialObject Objects Races Scenarios
+#do
+#echo "$dir"
+#mkdir -p "$2$dir"
+#./convert.sh "$1$dir" "$2$dir"
+#echo "[\"$dir\"] = {" > "$2/$dir.lua"
+#cat $2$dir/* >> "$2/$dir.lua"
+#echo "};" >> "$2/$dir.lua"
+#cat $2/$dir.lua >> $2/data.lua
+#done
 
 
 #convert sprites
@@ -25,7 +25,6 @@ mkdir -p $2/Sprites
 echo "[\"Sprites\"] = {" > $2/Sprites.lua
 find $1/Sprites/ -depth 1 -type d -print0 | xargs -0 -n 1 ./sprites.sh $2
 echo "};" >> $2/Sprites.lua
-
 
 cat $2/Sprites.lua >> $2/data.lua
 echo "};" >> $2/data.lua
--- sprites.sh
+++ sprites.sh
@@ -3,5 +3,34 @@ dirname=${2##.*/}
 number=${dirname%% *}
 name=${dirname#* }
 echo $dirname
-montage "$2"/*.png -geometry +0+0 -background transparent $1/Sprites/$number.png
+n=`ls -1 "$2" | wc -l`
+n=${n##* }
+case "$n" in 
+	1) dim=1x1;;
+	3) dim=3x1;;
+	5) dim=5x1;;
+	6) dim=3x2;;
+	7) dim=7x1;;
+	8) dim=4x2;;
+	9) dim=3x3;;
+	12) dim=4x3;;
+	15) dim=5x3;;
+	21) dim=7x3;;
+	24) dim=6x4;;
+	25) dim=5x5;;
+	27) dim=9x3;;
+	28) dim=7x4;;
+	29) dim=29x1;;
+	36) dim=6x6;;
+esac
+montage "$2/*.png" -tile 6x7 -geometry +0+0 -background transparent "$1"/Sprites/$number.png
 echo "[$number] = \"$name\";" >> $1/Sprites.lua
+
+cat > $1/Sprites/$number.xml <<EOF
+<?xml version="1.0"?>
+<sprite>
+	<dimensions x="${dim%x*}" y="${dim#*x}" />
+	<rotational />
+	<scale factor="1.0" />
+</sprite> 
+EOF

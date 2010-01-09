8549b3c91548dbf65c05563ab0f4f7167bd57ee2
Scott McClaugherty <gamefreak4321@gmail.com>
2010-01-09 15:11:00 -0500

    Uncommented some temperarily disabled code.
    Signed-off-by: Scott McClaugherty <gamefreak4321@gmail.com>

--- run.sh
+++ run.sh
@@ -7,17 +7,16 @@ mkdir -p $2
 echo "return {" > $2/data.lua
 
 #convert the XML
-
-#for dir in Actions InitialObject Objects Races Scenarios
-#do
-#echo "$dir"
-#mkdir -p "$2$dir"
-#./convert.sh "$1$dir" "$2$dir"
-#echo "[\"$dir\"] = {" > "$2/$dir.lua"
-#cat $2$dir/* >> "$2/$dir.lua"
-#echo "};" >> "$2/$dir.lua"
-#cat $2/$dir.lua >> $2/data.lua
-#done
+for dir in Actions InitialObject Objects Races Scenarios
+do
+echo "$dir"
+mkdir -p "$2$dir"
+./convert.sh "$1$dir" "$2$dir"
+echo "[\"$dir\"] = {" > "$2/$dir.lua"
+cat $2$dir/* >> "$2/$dir.lua"
+echo "};" >> "$2/$dir.lua"
+cat $2/$dir.lua >> $2/data.lua
+done
 
 
 #convert sprites
